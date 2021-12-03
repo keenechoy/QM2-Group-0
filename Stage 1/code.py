bd5614f7257eab6080b3006964bb4c5e317990a7
keenechoy <keenesee@gmail.com>
2021-12-03 01:29:28 +0000

    Adding hate_crime.csv and lines in code.py

--- Stage 1/code.py
+++ Stage 1/code.py
@@ -1,6 +1,17 @@
+# Importing packages
 import pandas as pd
+import matplotlib as plt
+import plotly.express as px
+import seaborn as sns
 
-path = '/Users/keenechoy/PycharmProjects/QM2-Group-0/Data/us-state-pop-by-race.csv'
-data = pd.read_csv(path, encoding='latin1')
+# Setting dataframes
+path1 = './Data/Stage 1/us-state-pop-by-race.csv'
+race_demo = pd.read_csv(path1, encoding='latin1')
 
-print(data.head())
+path2 = './Data/Stage 1/hate_crime.csv'
+crime_data= pd.read_csv(path2, encoding='latin1')
+
+# Cleaning crime_data to only contain what we need
+crime_data.head()
+
+
