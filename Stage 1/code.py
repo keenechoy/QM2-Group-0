cde8ed6ca01d4f7002f693d6b8cbe4670afe82a9
keenechoy <keenesee@gmail.com>
2021-12-08 11:29:31 +0000

    Progress in Stage 1 code.py

--- Stage 1/code.py
+++ Stage 1/code.py
@@ -1,17 +1,35 @@
 # Importing packages
 import pandas as pd
+import numpy
 import matplotlib as plt
 import plotly.express as px
 import seaborn as sns
-
 # Setting dataframes
-path1 = './Data/Stage 1/us-state-pop-by-race.csv'
-race_demo = pd.read_csv(path1, encoding='latin1')
+path1 = '/Users/keenechoy/PycharmProjects/QM2-Group-0/Data/Stage 1/us-state-pop-by-race.csv'
+race_demo = pd.read_csv(path1, low_memory=False)
 
-path2 = './Data/Stage 1/hate_crime.csv'
-crime_data= pd.read_csv(path2, encoding='latin1')
+path2 = '/Users/keenechoy/PycharmProjects/QM2-Group-0/Data/Stage 1/hate_crime.csv'
+crime_data = pd.read_csv(path2, low_memory=False)
 
-# Cleaning crime_data to only contain what we need
-crime_data.head()
+# Creating a 2020 AAHC dataframe
+aahc2020 = crime_data[crime_data['DATA_YEAR'] == 2020]
+aahc2020 = aahc2020[aahc2020['BIAS_DESC'] == 'Anti-Asian']
+aahc2020.reset_index(drop=True, inplace=True)
+
+# Assigning 'State' attribute for 'Federal' cases by looking at the ORI
+if aahc2020['ORI'] == 'CAFBILA00' or aahc2020['ORI'] == 'CAFBISC00':
+    aahc2020['STATE_NAME'] = 'California'
+
+# Merging with state dataset
+aahc2020.rename(columns={'STATE_NAME': 'State'}, inplace=True)
+race_demo.rename(columns={'Label': 'State'}, inplace=True)
+aahc2020 = aahc2020.merge(race_demo, left_on='State', right_on='State', how='left')
+
+# Removing collumns we don't need and cleaning data types
+aahc2020.drop(['PUB_AGENCY_UNIT', 'Anti-Asian Hate Crime 2020'], axis=1, inplace=True)
+
+#Counting number of AAHC by state
+statecount = aahc2020.value_counts(subset='State')
+statecount = statecount.to_frame()
 
 
